67b70409baa52f3162aa6f2df3c31c714e536060
Andrew Grapsas <aagrapsas@gmail.com>
2016-01-14 15:26:11 -0500

    Add alternate directory for execution

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var exec = require('child_process').exec
 
-function _command (cmd, cb) {
-  exec(cmd, { cwd: __dirname }, function (err, stdout, stderr) {
+function _command (cmd, cb, altDir) {
+  exec(cmd, { cwd: altDir ? altDir : __dirname }, function (err, stdout, stderr) {
     cb(stdout.split('\n').join(''))
   })
 }
 
 module.exports = { 
-    short : function (cb) { 
-      _command('git rev-parse --short HEAD', cb)
+    short : function (cb, altDir) { 
+      _command('git rev-parse --short HEAD', cb, altDir)
     }
-  , long : function (cb) { 
-      _command('git rev-parse HEAD', cb)
+  , long : function (cb, altDir) { 
+      _command('git rev-parse HEAD', cb, altDir)
     }
-  , branch : function (cb) { 
-      _command('git rev-parse --abbrev-ref HEAD', cb)
+  , branch : function (cb, altDir) { 
+      _command('git rev-parse --abbrev-ref HEAD', cb, altDir)
     }
-  , tag : function (cb) { 
-      _command('git describe --always --tag --abbrev=0', cb)
+  , tag : function (cb, altDir) { 
+      _command('git describe --always --tag --abbrev=0', cb, altDir)
     }
-  , log : function (cb) { 
+  , log : function (cb, altDir) { 
       _command('git log --no-color --pretty=format:\'[ "%H", "%s", "%cr", "%an" ],\' --abbrev-commit', function (str) {
         str = str.substr(0, str.length-1)
         cb(JSON.parse('[' + str + ']'))
-      })
+      }, altDir)
     }
 }
